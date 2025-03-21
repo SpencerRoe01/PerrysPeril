d8d3c7cc22a05bfc378658612813acf36c913f90
SpencerRoe01 <145682313+SpencerRoe01@users.noreply.github.com>
2025-03-21 01:13:13 -0500

    ignore

---
+++ Assets/Scripts/EnemyRoot.cs
@@ -0,0 +1,86 @@
+using Pathfinding;
+using UnityEngine;
+using System.Collections;
+
+public class EnemyRoot : MonoBehaviour
+{
+    public int Health;
+    public float MoveSpeed;
+    public bool IsStunned;
+    public float StunDuration;
+    public bool IsRoamingEnemy;
+
+    public Animator Animator;
+
+    
+
+    private void Start()
+    {
+        transform.parent.gameObject.GetComponent<AIPath>().maxSpeed = MoveSpeed;
+    }
+
+    private void Update()
+    {
+       
+        if (Health <= 0) 
+        {
+            IsStunned = true;
+        }
+        
+        if (IsStunned) 
+        {
+            Debug.Log("a");
+            //play stun animation
+
+
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            foreach (Behaviour behaviour in transform.parent.GetComponents<Behaviour>())
+            {
+                behaviour.enabled = true;
+
+            }
+            transform.parent.GetComponent<SpriteRenderer>().enabled = true;
+
+            Health = 1;
+            StartCoroutine(StunCoroutine(StunDuration));
+            IsStunned = false;
+            Debug.Log("g");
+            
+
+        }
+    }
+    public void PlayShootAnimation()
+    {
+        //play Shoot animation
+    }
+    void Die()
+    {
+        // play Die animation 
+        Destroy(gameObject);
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Projectile")
+        {
+            if (other.gameObject.GetComponent<Projectile>().isEnemyProjectile == false) 
+            {
+                Health -= 1;
+                
+                gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                foreach (Behaviour behaviour in transform.parent.GetComponents<Behaviour>())
+                {
+                    behaviour.enabled = false;
+
+                }
+                transform.parent.GetComponent<SpriteRenderer>().enabled = false;
+                other.gameObject.GetComponent<Projectile>().DestoyProjectile();           
+            }
+        }
+
+    }
+    private IEnumerator StunCoroutine(float duration)
+    {
+ 
+        yield return new WaitForSeconds(duration);
+    }
+    }
