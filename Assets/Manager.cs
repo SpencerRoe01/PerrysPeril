198dae0b7d887a8dafaa66053bbaef7c4bbcbca5
parks-spry <165302204+parks-spry@users.noreply.github.com>
2025-03-24 10:25:04 -0500

    Created SettingsMenu with HomeButton

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Manager : MonoBehaviour {
-    public void GameplayScene(){
+    public void StartScene(){
+        SceneManager.LoadScene(0);
+    }
+    public void SettingsScene(){
         SceneManager.LoadScene(1);
     }
+    public void GameplayScene(){
+        SceneManager.LoadScene(2);
+    }
 }
+    
