166bec92d18581c2b07103e696534cf944bdf469
SpencerRoe01 <145682313+SpencerRoe01@users.noreply.github.com>
2025-03-26 10:20:10 -0500

    Added explode Effect

--- Assets/Scripts/ExplodingEnemyScript.cs
+++ Assets/Scripts/ExplodingEnemyScript.cs
@@ -1,10 +1,15 @@
 
 using UnityEngine;
 using System.Collections;
+using Pathfinding;
 public class ExplodingEnemyScript : MonoBehaviour
 {
     public bool Exploding;
     public GameObject Particle;
+    public GameObject Explosion;
+
+
+   
     public void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.gameObject.tag == "Player" && transform.parent.Find("ExplodingEnemyRoot").GetComponent<EnemyRoot>().IsStunned == false)
@@ -12,7 +17,7 @@ public class ExplodingEnemyScript : MonoBehaviour
             StartCoroutine(Explode(3));
             Exploding = true;
             Particle.SetActive(true);
-
+            transform.parent.gameObject.GetComponent<AIPath>().maxSpeed = transform.parent.Find("ExplodingEnemyRoot").GetComponent<EnemyRoot>().MoveSpeed + 3;
 
         }
     }
@@ -20,7 +25,10 @@ public class ExplodingEnemyScript : MonoBehaviour
     {
 
         yield return new WaitForSeconds(duration);
+        Destroy(Instantiate(Explosion, transform.position,Quaternion.identity),3);
         transform.parent.transform.GetChild(0).GetComponent<EnemyRoot>().DestroyEnemy();
-        
+
+
+
     }
 }
