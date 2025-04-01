6b3447a031647fe0123342305c194051eff4a638
parks-spry <165302204+parks-spry@users.noreply.github.com>
2025-04-01 10:39:17 -0500

    Sound mananger link to settings menu

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour{
@@ -8,10 +9,21 @@ public class SoundManager : MonoBehaviour{
     public AudioSource SFXSource;
     public static int MusicVolume = 100;
     public static int SFXVolume = 100;
+    public Slider MSlider;
+    public Slider SSlider;
     void Awake(){
         DontDestroyOnLoad(this.gameObject);
     }
+    void Update(){
+        if(SceneManager.GetActiveScene().name == "Settings Menu"){
+            VolumeUpdate();
+        }
+    }
     void VolumeUpdate(){
+        MSlider = GameObject.Find("MusicSlider").GetComponent<Slider>();
+        MusicVolume = (int) MSlider.value;
+        SSlider = GameObject.Find("SFXSlider").GetComponent<Slider>();
+        SFXVolume = (int) SSlider.value;
         MusicSource.volume = MusicVolume/100f;
         SFXSource.volume = SFXVolume/100f;
     }
