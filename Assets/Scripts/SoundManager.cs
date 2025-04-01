d8da93f7f650d35b083b75ed28a7fac8e407dce0
parks-spry <165302204+parks-spry@users.noreply.github.com>
2025-04-01 09:51:42 -0500

    Update SoundManager.cs

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,10 +6,13 @@ public class SoundManager : MonoBehaviour{
     public Sound[] SFX;
     public AudioSource MusicSource;
     public AudioSource SFXSource;
+    public static int MusicVolume = 100;
+    public static int SFXVolume = 100;
     void Awake(){
         DontDestroyOnLoad(this.gameObject);
     }
     void VolumeUpdate(){
-        MusicSource.volume = GameObject.Find("MusicSlider").GetComponent<Slider>().value;
+        MusicSource.volume = MusicVolume/100f;
+        SFXSource.volume = SFXVolume/100f;
     }
 }
